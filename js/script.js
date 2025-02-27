0e9e529502596169ac33912fff5dc029bbbf613a
csrijon <csrijon92@gmail.com>
2025-02-28 00:26:59 +0530

    new dhamaka

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",() => {
+  let menuicon = document.querySelector('.menu-icon');
+menuicon.addEventListener("click",() => {
+  let navmenu = document.querySelector(".nav-menu");
+  navmenu.classList.toggle('active');
+})
+}
+)
+
